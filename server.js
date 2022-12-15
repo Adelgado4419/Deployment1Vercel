f4757429976da6d7a28299013fdfb0989c078813
Alexis Delgado <alexisdelgado@MacBook-Pro-7.localdomain>
2022-12-14 19:04:12 -0800

    added variable service port with env file and added env file to gitignore

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.listen(3000)
+app.listen(PORT, () => console.log(`Server is running in port ${3000}`))
 
 
 module.exports = app;
